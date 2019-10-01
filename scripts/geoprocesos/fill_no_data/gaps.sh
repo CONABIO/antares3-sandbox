bb1670556221b5107dfb328f5a8867734f7e81e2
robmartz <rmartinez@conabio.gob.mx>
2019-10-01 13:07:45 -0500

    Adding menu options

--- scripts/geoprocesos/fill_no_data/gaps.sh
+++ scripts/geoprocesos/fill_no_data/gaps.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+show_options() {
+    # if no param is given, display a menu and then exit
+    (( $# < 1 )) && {
+        # display a menu of options
+        echo " "
+        echo "            MAIN MENU"
+        echo
+        echo "       -p                Path to scenes"
+        echo "       -d                Delete original scenes after fill-nodata completes"
+    }
+}
+
 function get_scene (){
-  echo "Lee escena"
+  echo "Lee escenas de $1"
 }
 
 function fill_nodata (){
@@ -13,9 +25,15 @@ function delete_original (){
 }
 
 main() {
-    get_scene "$@"
-    fill_nodata
-    delete_original "$@"
+
+  if [[ $# -eq 0 ]] ; then
+    show_options
+    exit 0
+  fi
+
+  get_scene "$@"
+  fill_nodata
+  delete_original "$@"
 }
 
 main "$@"
