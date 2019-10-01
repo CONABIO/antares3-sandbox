5746c24932229ffdc765568c21dffd445a6b7963
Roberto Martínez <rmartinez@conabio.gob.mx>
2019-10-01 20:30:59 +0000

    Adding control functions

--- scripts/geoprocesos/fill_no_data/gaps.sh
+++ scripts/geoprocesos/fill_no_data/gaps.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#path=$(pwd)
 delete=n
 
 function usage()
@@ -11,7 +10,7 @@ function usage()
     printf "Usage: Some example here\n"
     printf "\n"
     printf "  -h --help\t\tPrints this help and exits\n"
-    printf "  -p --path\tPath to Landsat 7 scenes (default $path)\n"
+    printf "  -p --path\tPath to Landsat 7 scenes \n"
     printf "  -d --delete\tDelete original scenes (default off)\n"
 		printf "\n"
 }
@@ -64,11 +63,37 @@ while true; do
     esac
 done
 
-# handle non-option arguments
-if [[ -z "$path" ]]; then
-    usage
-    printf "[ERROR] A path to scene(s) is required.\n"
-    exit 4
-fi
+function check_path_param(){
+  # handle non-option arguments
+  if [[ -z "$path" ]]; then
+      usage
+      printf "[ERROR] A path to scene(s) is required.\n"
+      exit 4
+  fi
+  if [ -d "$path" ]; then
+    # Take action if $path exists
+    fill_nodata_scene "$@"
+  else
+    #  Control will jump here if $path does NOT exists 
+    echo "Error: ${path} not found. Can not continue."
+    exit 1
+  fi
+}
 
-echo "Procesing in "$path 
+function fill_nodata_scene(){
+  echo echo "Processing scenes in ${path}..."
+}
+
+function delete_original(){
+  # Delete the unzipped file after running sen2cor
+  if [ "$delete" == "y" ]; then
+     echo "deleting original scenes"
+  fi
+}
+
+main() {
+    check_path_param "$@"
+    delete_original "$@"
+}
+
+main "$@"
