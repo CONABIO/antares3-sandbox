af87871b00da7aa34fd1108992c05c7efc7bbfa2
ixime <ixmezach@googlemail.com>
2019-10-16 15:12:08 -0500

    test adding items with ministac-admin

---
+++ scripts/ministac_add_items_test.sh
@@ -0,0 +1,13 @@
+# From ministac-admin in https://github.com/loicdtx/ministac-admin we tested add compressed scenes and uncompressed
+# The following is the dry run to add uncompressed scenes 
+
+add_items.py /datos/unzipped_test_l7/ --collection landsat_sr_7 --parser landsat --dryrun
+
+#### output ####
+# /datos/unzipped_test_l7/LE070180452014040501T1-SC20190926174934
+# OK
+# /datos/unzipped_test_l7/LE070180452014030401T1-SC20191008100005
+# OK
+# /datos/unzipped_test_l7/LE070180452014021601T1-SC20190926013754
+# OK
+
